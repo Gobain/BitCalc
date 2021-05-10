31ed2cc5cbb7cd3650c838feb5de1a755938d3d2
Gobain <50881995+Gobain@users.noreply.github.com>
2021-05-10 12:35:16 -0300

    New layout
    New layout and correction of a bug that do not show the result with the
    form.

--- BitCalc.js
+++ BitCalc.js
@@ -1,22 +1,57 @@
-function CalcProfit(){
+var valorAplicado
+var wallBTC
+var bitBTC
+var profit
+var showResult
+var btcRecebido
 
-  var valorAplicado = document.getElementById("BRL").value * 1.0
-  var precoDoBTC = document.getElementById("BTCPrice").value * 1.0
-  var lucro = document.getElementById("Profit").value * 1.0
-  //window.alert(lucro)
+function start() {
+
+  valorAplicado = document.getElementById("BRL").value * 1.0
+  //wallBTC = document.getElementById("WallBTC").value * 1.0
+  bitBTC = document.getElementById("BitBTC").value * 1.0
+  profit = document.getElementById("Profit").value * 1.0
+
+
+  if (wallBTC && bitBTC != null) {
+   calcProfit()
+  }
+  else if (valorAplicado && bitBTC && profit != null) {
+    calcWallBTC()
+  }
+
+}
+
+function calcWallBTC() {
 
   //valor descontando taxa da Bitpreço => 0.2%
   valorDescontado = valorAplicado - (valorAplicado * 0.2 / 100)
-  //window.alert(valorDescontado)
 
-  var btcRecebido = ((valorDescontado) / (precoDoBTC / 100_000_000.00)) / 100_000_000.00
+
+  btcRecebido = ((valorDescontado) / (bitBTC / 100_000_000.00)) / 100_000_000.00
 
   //Cálculo do novo preço de venda do Bitcoin
-  var novoPrecoBTC = (valorAplicado + (valorAplicado * lucro / 100)) / btcRecebido
-  //window.alert("novo preco BTC " + novoPrecoBTC)
+  wallBTC = (valorAplicado + (valorAplicado * profit / 100)) / btcRecebido
 
-  //Adição de 1 centavo para evitar erros
-  novoPrecoBTC += 0.01
+  //Adição de 1 centavo e 1 satoshi para evitar erros
+  wallBTC += 0.01
+  btcRecebido += 0.00000001
 
-  document.write("<p>Você deve vender " + btcRecebido + " BTC " + "na Walltime por: " + novoPrecoBTC + " Reais </p>")
+  //document.getElementById('WallBTC').value = wallBTC.toFixed(2)
+  showResult = document.getElementById('QuantBTC')
+  showResult.innerHTML = "Voce dever vender: " + "<output id=\"receivedBTC\">" + btcRecebido.toFixed(8) + " BTC "  + "Na Walltime por: R$" + wallBTC.toFixed(2)
+
+  //document.getElementById('QuantBTC').appendChild(btcRecebido.toFixed(8))
+}
+
+function calcProfit() {
+
+  showResult = ((wallBTC - bitBTC) * 100) / bitBTC
+  document.getElementById('Profit').value = showResult.toFixed(2) + "%"
+}
+
+function copyBTC(){
+  document.getElementById('receivedBTC').select()
+
+  document.execCommand("copy");
 }
